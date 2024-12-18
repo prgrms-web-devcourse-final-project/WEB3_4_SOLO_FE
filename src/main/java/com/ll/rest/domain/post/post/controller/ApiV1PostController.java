e4c477dddcae0dfeada509ad8fe171fe50944c68
jhs512 <jangka512@gmail.com>
2024-12-18 15:10:54 +0900

    valid

--- src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
@@ -3,7 +3,10 @@ package com.ll.rest.domain.post.post.controller;
 import com.ll.rest.domain.post.post.entity.Post;
 import com.ll.rest.domain.post.post.service.PostService;
 import com.ll.rest.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -45,7 +48,11 @@ public class ApiV1PostController {
 
 
     record PostModifyReqBody(
+            @NotBlank
+            @Length(min = 2)
             String title,
+            @NotBlank
+            @Length(min = 2)
             String content
     ) {
     }
@@ -54,7 +61,7 @@ public class ApiV1PostController {
     @Transactional
     public RsData modifyItem(
             @PathVariable long id,
-            @RequestBody PostModifyReqBody reqBody
+            @RequestBody @Valid PostModifyReqBody reqBody
     ) {
         Post post = postService.findById(id).get();
 
