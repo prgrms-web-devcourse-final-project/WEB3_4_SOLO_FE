9595a3f68b2bae30ec190e05db8c2f36f0d080c7
jhs512 <jangka512@gmail.com>
2024-12-19 10:25:02 +0900

    Dto 1

--- src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
+++ src/main/java/com/ll/rest/domain/post/post/dto/PostDto.java
@@ -8,15 +8,15 @@ import java.time.LocalDateTime;
 @Getter
 public class PostDto {
     private long id;
-    private LocalDateTime createDate;
-    private LocalDateTime modifyDate;
+    private LocalDateTime createdDatetime;
+    private LocalDateTime modifiedDatetime;
     private String title;
     private String content;
 
     public PostDto(Post post) {
         this.id = post.getId();
-        this.createDate = post.getCreateDate();
-        this.modifyDate = post.getModifyDate();
+        this.createdDatetime = post.getCreateDate();
+        this.modifiedDatetime = post.getModifyDate();
         this.title = post.getTitle();
         this.content = post.getContent();
     }
