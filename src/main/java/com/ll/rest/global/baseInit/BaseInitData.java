e06a0202fa6ed78ab2ef58a181fa3c41be361194
jhs512 <jangka512@gmail.com>
2024-12-20 14:13:58 +0900

    Member author

--- src/main/java/com/ll/rest/global/baseInit/BaseInitData.java
+++ src/main/java/com/ll/rest/global/baseInit/BaseInitData.java
@@ -44,8 +44,11 @@ public class BaseInitData {
     public void work2() {
         if (postService.count() > 0) return;
 
-        Post post1 = postService.write("축구 하실 분?", "14시 까지 22명을 모아야 합니다.");
-        Post post2 = postService.write("배구 하실 분?", "15시 까지 12명을 모아야 합니다.");
-        Post post3 = postService.write("농구 하실 분?", "16시 까지 10명을 모아야 합니다.");
+        Member memberUser1 = memberService.findByUsername("user1").get();
+        Member memberUser2 = memberService.findByUsername("user2").get();
+
+        Post post1 = postService.write(memberUser1, "축구 하실 분?", "14시 까지 22명을 모아야 합니다.");
+        Post post2 = postService.write(memberUser1, "배구 하실 분?", "15시 까지 12명을 모아야 합니다.");
+        Post post3 = postService.write(memberUser2, "농구 하실 분?", "16시 까지 10명을 모아야 합니다.");
     }
 }
