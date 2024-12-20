3e6dd780b0ad4f04b41d55e922196d5c3242548c
jhs512 <jangka512@gmail.com>
2024-12-20 10:33:51 +0900

    member 1

--- src/main/java/com/ll/rest/global/baseInit/BaseInitData.java
+++ src/main/java/com/ll/rest/global/baseInit/BaseInitData.java
@@ -13,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Configuration
 @RequiredArgsConstructor
 public class BaseInitData {
+    private final MemberService memberService;
     private final PostService postService;
     @Autowired
     @Lazy
@@ -22,11 +23,23 @@ public class BaseInitData {
     public ApplicationRunner baseInitDataApplicationRunner() {
         return args -> {
             self.work1();
+            self.work2();
         };
     }
 
     @Transactional
     public void work1() {
+        if (memberService.count() > 0) return;
+
+        Member memberSystem = memberService.join("system", "1234", "시스템");
+        Member memberAdmin = memberService.join("admin", "1234", "관리자");
+        Member memberUser1 = memberService.join("user1", "1234", "유저1");
+        Member memberUser2 = memberService.join("user2", "1234", "유저2");
+        Member memberUser3 = memberService.join("user3", "1234", "유저3");
+    }
+
+    @Transactional
+    public void work2() {
         if (postService.count() > 0) return;
 
         Post post1 = postService.write("축구 하실 분?", "14시 까지 22명을 모아야 합니다.");
